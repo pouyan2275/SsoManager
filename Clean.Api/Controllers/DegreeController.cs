ea7ea381ceaaac009eff12a6631ef80cd5e2c3aa
pouyan alizadeh <pouyan9913@gmail.com>
2024-10-04 19:52:10 +0330

    Initial commit

---
+++ Clean.Api/Controllers/DegreeController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dtos.Degrees;
+using Domain.Entities;
+using Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Personal.Server.Bases.Controllers;
+
+namespace Personal.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DegreeController : CrudController<DegreeDto, DegreeDtoSelect, Degree>
+    {
+        private readonly IDegreeService _crudService;
+
+        public DegreeController(IDegreeService crudService) : base(crudService)
+        {
+            _crudService = crudService;
+        }
+    }
+}
